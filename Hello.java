0a57035efe16994bd65fca2c0cdd22eba93936eb
Omsolkar <202602776+Omsolkar@users.noreply.github.com>
2026-03-05 14:57:53 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-      piublic static void main (String[] args)
+      public static void main (String[] args)
        {
           System.out.println("Hello World 12345");
   }
